9e3f87f4a9ea24b1cd1f15dc07754b868033e91a
lili-dudas <123129465+lili-dudas@users.noreply.github.com>
2023-02-06 01:18:42 -0500

    web build

--- Assets/supportScripts/LoadNewScene.cs
+++ Assets/supportScripts/LoadNewScene.cs
@@ -8,7 +8,7 @@ public class LoadNewScene : MonoBehaviour
 
     public void LoadScene1()
     {
-        SceneManager.LoadScene("Main-ApplePicker");
+        SceneManager.LoadScene("ApplePicker_LD");
     }
 
     public void LoadScene2()
