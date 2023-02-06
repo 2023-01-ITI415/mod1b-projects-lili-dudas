d3845e69d55076434a4c2ab4a3920dc8bff65a13
lili-dudas <123129465+lili-dudas@users.noreply.github.com>
2023-02-06 02:15:14 -0500

    reverse

--- Assets/supportScripts/LoadNewScene.cs
+++ Assets/supportScripts/LoadNewScene.cs
@@ -8,7 +8,7 @@ public class LoadNewScene : MonoBehaviour
 
     public void LoadScene1()
     {
-        SceneManager.LoadScene("ApplePicker_LD");
+        SceneManager.LoadScene("Main-ApplePicker");
     }
 
     public void LoadScene2()
