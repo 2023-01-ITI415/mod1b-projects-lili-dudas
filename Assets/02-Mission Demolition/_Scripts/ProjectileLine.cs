02279b5b116f40c2dbbc767d48665de7dff9b1f4
lili-dudas <123129465+lili-dudas@users.noreply.github.com>
2023-02-09 14:51:37 -0500

    add ProjectileLine edits

--- Assets/02-Mission Demolition/_Scripts/ProjectileLine.cs
+++ Assets/02-Mission Demolition/_Scripts/ProjectileLine.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class ProjectileLine : MonoBehaviour
 {
+    static List <ProjectileLine> PROJ_LINES = new List<ProjectileLine>();
+    private const float DIM_MULT = 0.75f;
+
     private LineRenderer _line;
     private bool _drawing = true;
     private Projectile _projectile;
@@ -16,6 +19,7 @@ public class ProjectileLine : MonoBehaviour
         _line.positionCount = 1;
         _line.SetPosition(0, transform.position);
         _projectile = GetComponentInParent<Projectile>();
+        ADD_LINE( this );
     }
 
     void FixedUpdate()
@@ -31,5 +35,22 @@ public class ProjectileLine : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void OnDestroy(){
+        //remove this ProjectileLine from PROJ_LINES
+        PROJ_LINES.Remove( this );
+    }
+
+    static void ADD_LINE(ProjectileLine newLine){
+        Color col;
+        //iterate over all the old lines and dim them
+        foreach (ProjectileLine pl in PROJ_LINES){
+            col = pl._line.startColor;
+            col = col * DIM_MULT;
+            pl._line.startColor = pl._line.endColor = col;
+        }
+        //add newLine to the List
+        PROJ_LINES.Add( newLine );
     }
 }
