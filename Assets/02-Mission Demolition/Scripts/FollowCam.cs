1ac3c2ae17890fbd2f8c009edbc8c3e462adc724
lili-dudas <123129465+lili-dudas@users.noreply.github.com>
2023-02-09 01:46:31 -0500

    issue c

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; //static point of interest
+    [Header("Inscribed")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Dynamic")]
     public float camZ; //desired Z pos of the camera
@@ -19,6 +22,9 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return;
         //get the position of the poi
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x,destination.x);
+        destination.y = Mathf.Max(minXY.y,destination.y);
+        destination = Vector3.Lerp(transform.position,destination, easing);
         //force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         //set camera to the destination
